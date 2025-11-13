1951c8a27379b6971121497f47edab8bd677ca73
rafael-iste <rafael.iste--gaie@edu.esiee.fr>
2025-11-13 12:59:36 +0000

    Solution de l'exercice 04-primes sur les nombres premiers

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
+"""
+04-primes
+
+Contient une fonction secondaire isprime permettant de tester si un entier est premier,
+et une fonction principale affichant les nombres premiers inférieurs à 100 pour tester isprime.
+"""
+
 from math import sqrt
 
-#### Fonction secondaire
-
-
 def isprime(p):
+    """
+    Vérifie si un entier est un nombre premier.
 
-    # votre code ici
+    Un nombre premier est un entier naturel supérieur ou égal à 2
+    qui n'admet que deux diviseurs positifs distincts : 1 et lui-même.
 
-    pass
+    @param p: Entier à tester.
+    @type p: int
+    @return: True si p est un nombre premier, False sinon.
+    @rtype: bool
+    """
+    if p < 2:
+        return False
+    if p == 2:
+        return True
+    if p % 2 == 0:
+        return False
 
-#### Fonction principale
-
+    limit = int(sqrt(p)) + 1
+    for i in range(3, limit, 2):
+        if p % i == 0:
+            return False
+    return True
 
 def main():
+    """
+    Fonction principale du programme.
 
-    # vos appels à la fonction secondaire ici
+    Affiche la liste des nombres premiers inférieurs à 100 afin
+    de vérifier le bon fonctionnement de la fonction secondaire.
+    """
 
     for n in range(100):
         if isprime(n):
